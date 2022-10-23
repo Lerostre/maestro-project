7963d12ffbd28077bf9e89554e00d7ac03af75d6
Lerostre <backup.ml322@gmail.com>
2022-10-24 02:38:13 +0300

    done and ready

---
+++ app.py
@@ -0,0 +1,77 @@
+﻿import sqlite3
+import pandas as pd
+import numpy as np
+import ast
+import os
+from web_search import *
+from flask import Flask, render_template, request, url_for, flash, redirect
+from werkzeug.exceptions import abort
+from flask_paginate import Pagination, get_page_args
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+
+meta = pd.read_csv("sents_meta.csv")
+sents = pd.read_csv("parsed_sents.csv")
+sents = sents.applymap(lambda x: ast.literal_eval(x))
+words = pd.read_csv("morph_info.csv")
+
+def get_users(res, offset=0, per_page=10):
+    return res[offset: offset + per_page]
+
+
+@app.route('/', methods=('GET','POST'))
+def index():
+
+    if request.method == 'POST':
+        data = request.form['query']
+        return redirect(url_for('search2', query=data))
+
+    else:
+        html = render_template('index.html')
+        return html
+
+@app.route('/search2/', defaults={'query': None}, methods=['GET', 'POST'])
+@app.route('/search2/<query>', methods=['GET', 'POST'])
+def search2(query):
+
+    if request.method == 'POST':
+        data = request.form['query']
+        return redirect(url_for('search2', query=data))
+
+    if not query:
+        html = render_template('create.html', pagination=False, total=0)
+        return html
+
+    results = search(query, sents, words, meta)
+
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                    per_page_parameter='per_page')
+    per_page = 10
+    total = len(results)
+    pagination_users = get_users(results, offset=offset, per_page=per_page)
+    print(total)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    if total % 2 != 0:
+        pagination_users.append([])
+    items = list(zip(pagination_users[0::2], pagination_users[1::2]))
+    html = render_template('create.html',
+                            page=page,
+                            query=query,
+                            items=items,
+                            per_page=10,
+                            pagination=pagination,
+                            total=total,
+                            highlight=highlight
+                            )
+    return html
+
+
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+if __name__ == '__main__':
+    app.run()
---
+++ web_search.py
@@ -0,0 +1,137 @@
+﻿import numpy as np
+import re
+from functools import reduce
+
+from rnnmorph.predictor import RNNMorphPredictor
+
+predictor = RNNMorphPredictor(language="ru")
+
+
+def consecutive(x):
+    if len(x) == 2:
+        # если два, то вырождается
+        return 1 if x[-1] - x[0] == 1 else 0
+    if len(x) > 2:
+        return 1 if (x[0] < x[-1]) and (len(x)*(x[0]+x[-1])/2 == sum(x)) else 0
+
+def search(request, sent_df, word_df, meta_df):
+    sub_req = request.split()
+    info = []
+
+    el_chars = {}
+    for element in sub_req:
+        with_pos = False
+        if element[0] == '"':
+            mode = "tokens"
+        elif element[0].isascii():
+            mode = "poss"
+        else:
+            mode = "lemmas"
+        if "+" in element:
+            with_pos = True
+        print(element, mode)
+        el_chars.update({element: word_search(element=element,
+                                              sent_df=sent_df,
+                                              word_df=word_df,
+                                              mode=mode,
+                                              with_pos=with_pos)})
+        
+    if len(el_chars) == 1:
+        sents, word_indices = list(el_chars[request].keys()), list(el_chars[request].values())
+        for sent in sents:
+            new_info = extract_info(sent, word_indices, meta_df)
+            if new_info:
+                info.append(new_info)
+
+    else:
+        common_sents = list(reduce(lambda x, y: x & y.keys(), el_chars.values()))
+
+        for sent in common_sents:
+            listik = []
+            for key in el_chars:
+                listik.append(el_chars[key][sent])
+            grid = np.array(np.meshgrid(*listik)).T.reshape(-1, len(el_chars))
+            diffs = np.apply_along_axis(consecutive, arr=grid, axis=1)
+            word_indices = diffs[np.where(diffs == 1)]
+            if word_indices.size > 0:
+                info.append(extract_info(sent, word_indices, meta_df))
+
+    return info
+
+
+def replace_keep_case(word, replacement, text):
+    def func(match):
+        g = match.group()
+        if g.islower(): return replacement.lower()
+        if g.istitle(): return replacement.title()
+        if g.isupper(): return replacement.upper()
+        return replacement      
+    return re.sub(rf"\b{word}\b", func, text, flags=re.I)
+
+
+def highlight(sentence, string):
+    if string[0] == '"':
+        string = string[1:-1]
+    if string[0].isascii():
+        return sentence
+    else:
+        return replace_keep_case(string, f'<mark>{string}</mark>', sentence)
+
+
+def highlight2(sentence, string):
+    red = "\033[31m"
+    nul = "\033[0m"
+    sentence = re.sub(
+        r"\b({})\b".format(re.escape(string)),
+        r"{}\1{}".format(red, nul),
+        sentence,
+        flags=re.I,
+    )
+    return sentence
+
+
+def extract_info(sent_indice, word_indice, meta_df):
+    if sent_indice:
+        row = meta_df.iloc[sent_indice]
+        return {"index": str(sent_indice), "sents": row.sentence,
+               "titles": row.title, "dates": row.date}
+    else:
+        return None
+    
+    
+def word_search(element, sent_df, word_df, mode="tokens", with_pos=False):
+    
+    if mode == "lemmas":
+        element = predictor.predict([element])[0].normal_form
+        print(element)
+        
+    elif mode == "tokens":
+        element = element[1:-1]
+
+    if with_pos:
+        indices = []
+        word, pos = element.split("+")
+        word_indices = word_search(word, sent_df, word_df, mode)
+        pos_indices = word_search(pos, sent_df, word_df, mode="poss")
+        indices_candidates = word_indices.keys() & pos_indices.keys()
+        morph_indices = []
+        for sent in indices_candidates:
+            candidate = list(set(word_indices[sent]) & set(pos_indices[sent]))
+            if candidate:
+                indices.append(sent)
+                morph_indices.append(candidate)
+
+    else:
+        v = np.vectorize(lambda x: element in x)
+        indices = np.where(v(sent_df[mode]) == True)[0]
+        row = sent_df.iloc[indices]
+        morph_indices = row[mode].apply(lambda x: x[element])
+        if with_pos:
+        #print(row[mode].apply(lambda x: x[word]))  # индексы находятся списками
+            morph = word_df.iloc[morph_indices.sum()]
+            u = np.vectorize(lambda x: x == pos)
+
+    if not any(morph_indices):
+        indices = []
+
+    return dict(zip(indices, morph_indices))
